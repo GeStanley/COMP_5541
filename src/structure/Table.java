80ea87826dbf202b1126001f916c04548d21f6c3
GeStanley <geoffrey.stanley@gmail.com>
2014-01-18 10:46:51 -0500

    changed the table.class code

--- src/structure/Table.java
+++ src/structure/Table.java
@@ -1,31 +1,129 @@
 package structure;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Table {
 	
-	private int row;
-    private int column;
-    private Cell[][]myTable;
+    private Cell[][] cells;
 	
     
     public Table(){
-		this.row=1;
-		this.column=1;
-		for(int rowCounter=0;rowCounter<11;rowCounter++){
-			for(int columnCounter=0;columnCounter<10;columnCounter++)
-				myTable[rowCounter][columnCounter]=new Cell();
-		}
+    	this.cells = new Cell[0][0];
 	}
     
+    public void defineArraySize(int rowQty, int columnQty){
+    	int rows = cells.length;
+    	int columns = cells[0].length;
+    	
+    	Cell[][] temp = new Cell[rows][columns];
+    	
+    	for(int i=0;i<rows;i++)
+    		for(int j=0;j<columns;j++)
+    			temp[i][j]=cells[i][j];
+    	
+    	cells = new Cell[rowQty][columnQty];
+    	
+    	for(int i=0;i<rows;i++)
+    		for(int j=0;j<columns;j++)
+    			cells[i][j]=temp[i][j];
+    }
     
-    public void set_cell(int row, int column, String format, String formula ){
-    	myTable[row][column].set_format(format);
-    	myTable[row][column].set_formula(formula);
+    public void insertRow(){
+    	int rows = cells.length;
+    	int columns = cells[0].length;
+    	
+    	Cell[][] temp = new Cell[rows][columns];
+    	
+    	for(int i=0;i<rows;i++)
+    		for(int j=0;j<columns;j++)
+    			temp[i][j]=cells[i][j];
+    	
+    	cells = new Cell[rows+1][columns];
+    	
+    	for(int i=0;i<rows;i++)
+    		for(int j=0;j<columns;j++)
+    			cells[i][j]=temp[i][j];
     }
-    public void set_cell(int row,int column,double myValue){
-    	myTable[row][column].setValue(myValue);
+    
+    public void insertColumn(){
+    	int rows = cells.length;
+    	int columns = cells[0].length;
+    	
+    	Cell[][] temp = new Cell[rows][columns];
+    	
+    	for(int i=0;i<rows;i++)
+    		for(int j=0;j<columns;j++)
+    			temp[i][j]=cells[i][j];
+    	
+    	cells = new Cell[rows][columns+1];
+    	
+    	for(int i=0;i<rows;i++)
+    		for(int j=0;j<columns;j++)
+    			cells[i][j]=temp[i][j];
     }
-    public Cell get_cell(int row, int column){
-    	return myTable[row][column];
+    
+    public int getRowIndex(char row){
+    	return ((int) row) - ((int) 'A');
+    }
+    
+    public Cell getCell(int row, char column) throws NullCellPointer{
+    	return cells[row][getRowIndex(column)];
+    }
+    
+    public Cell getCell(int row, int column) throws NullCellPointer{
+    	return cells[row][column];
+    }
+    
+    public void loadFromFile(String fileName) throws FileNotFoundException{
+    	Scanner input = new Scanner(fileName);
+    	
+    	int rows=1;
+    	
+    	String row = input.next();
+    	String[] data = row.split(";");
+    	
+    	int columns = data.length;
+    	
+    	while(input.hasNext()){
+    		input.next();
+    		rows++;
+    	}
+    	
+    	cells = new Cell[rows][columns];
+    	
+    	input.close();
+    	input = new Scanner(fileName);
+    	
+    	while(input.hasNext()){
+    		row = input.next();
+    		data = row.split(";");
+    		
+    		for(int i=0;i<data.length;i++){
+    			String[] cellData = data[i].split(",");
+    			Cell cell = getCell(Integer.parseInt(cellData[0]),Integer.parseInt(cellData[1]));
+    			cell.setValue(cellData[2]);
+    			cell.setFormula(cellData[3]);
+    		}
+    	}
+    	
+    	input.close();
+    }
+    
+    public void saveToFile(String fileName){
+    	PrintWriter output = new PrintWriter(fileName);
+    	
+    	for(int i=0;i<cells.length;i++){
+    		String data;
+    		for(int j=0;j<cells[i].length;j++)
+    			data = data + i + "," + j + "," + getCell(i,j).getValue() + "," + getCell(i,j).getFormula() + ";";
+    		
+    		output.println(data);
+    		data=null;
+    	}
+
     }
     
 }
