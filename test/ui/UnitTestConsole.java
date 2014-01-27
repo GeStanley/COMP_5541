c855c29c82cdfec7f25e59d7fd0a7d18d4f8184b
Sofian Benaissa <me@sfyn.net>
2014-01-27 01:00:09 -0500

    Experiment with console testing

--- test/ui/UnitTestConsole.java
+++ test/ui/UnitTestConsole.java
@@ -2,13 +2,26 @@ package ui;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import org.junit.Test;
 import structure.Cell;
 import structure.Table;
+import org.junit.After;
 
 public class UnitTestConsole {
+		
+	/**
+	 * Reset the system streams
+	 */
+	@After
+	public void after() {
+		System.setIn(System.in);
+		System.setOut(System.out);
+	}
 	
 	@Test
 	public void testCreateNew() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
@@ -36,20 +49,34 @@ public class UnitTestConsole {
 	    helpMethod.invoke(Console.class);
 	}
 	
+	/**
+	 * Test console save and load files
+	 */
 	@Test
-	public void testOpen() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-	    Method openMethod = getMethodOfClass(Console.class, "open");
-	    openMethod.invoke(Console.class);
+	public void testSave() {
+		String[] outputLines;
+		String testRow;
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		
+		// perform the save operation
+		System.setIn(new ByteArrayInputStream("A1\n1.0\nC1\nA1*3\nsave\n./tmp.csv\nquit".getBytes()));
+		System.setOut(new PrintStream(output));
+		Console.main(new String[0]);
+		
+		outputLines = output.toString().split("\n");
+		testRow = outputLines[outputLines.length-14];
+		assertEquals("Success: file saved", outputLines[outputLines.length-2]);
+
+		// perform the load op
+		output = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(output));
+		System.setIn(new ByteArrayInputStream("open\nquit\n".getBytes()));
+		Console.main(new String[0]);
+		outputLines = output.toString().split("\n");
+		assertEquals("Success: file loaded", outputLines[outputLines.length-1]);
+		assertEquals(testRow, outputLines[outputLines.length-1]);
 	}
 	
-	@Test
-	public void testSave() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-	    Method saveMethod = getMethodOfClass(Console.class, "save");
-	    saveMethod.invoke(Console.class);
-	}
-
-	
-
 	private Method getMethodOfClass(Class argClass, String argMethodName) {
 	    Method[] methods = argClass.getDeclaredMethods();
 	    for (Method method : methods) {
