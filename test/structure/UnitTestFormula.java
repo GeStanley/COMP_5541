b006476efef655fad5252daca6605663a6b0f6b5
Sofian Benaissa <me@sfyn.net>
2014-01-27 12:13:00 -0500

    Use before and after functions

--- test/structure/UnitTestFormula.java
+++ test/structure/UnitTestFormula.java
@@ -1,14 +1,12 @@
 package structure;
+
 import static org.junit.Assert.*;
-
 import java.lang.reflect.Method;
-
-import org.junit.Test;
-
+import org.junit.*;
 import structure.*;
 
 /**
- * This Junit class does a unit test on the Grammar class 
+ * This Junit class does a unit test on the Formula class 
  * to ensure that there are no errors using it as a
  * 'black box' for grammar
  * 
@@ -20,10 +18,31 @@ public class UnitTestFormula {
 	Formula tester;
 	Table table;
 	
+	/**
+	 * Pre conditions for tests
+	 */
+	@Before
+	public void before() {
+		table = new Table(5,5);
+		tester = new Formula(null, table);
+	}
 	
+	/**
+	 * Post conditions for tests
+	 */
+	public void after() {
+		table = null;
+		tester = null;
+	}
+	
+	
+	/**
+	 * Test all private methods in one go
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testPrivateMethodsFormula() throws Exception{
-		Formula testPrivate = new Formula();
 		boolean resultBool;
 		double resultDouble;
 		String methodName;
@@ -35,89 +54,89 @@ public class UnitTestFormula {
 		methodName = "inNum";
 		testMe = getMethodOfClass(structure.Formula.class, methodName, params);		
 		//Should passs
-		resultBool = (boolean) testMe.invoke(testPrivate, '9');
+		resultBool = (boolean) testMe.invoke(tester, '9');
 		assertTrue(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '0');
+		resultBool = (boolean) testMe.invoke(tester, '0');
 		assertTrue(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '4');
+		resultBool = (boolean) testMe.invoke(tester, '4');
 		assertTrue(resultBool);
 		//Should fail
-		resultBool = (boolean) testMe.invoke(testPrivate, 'a');
+		resultBool = (boolean) testMe.invoke(tester, 'a');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '-');
+		resultBool = (boolean) testMe.invoke(tester, '-');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '~');
+		resultBool = (boolean) testMe.invoke(tester, '~');
 		assertFalse(resultBool);
 		
 		//Test isLetter
 		methodName = "isLetter";
 		testMe = getMethodOfClass(structure.Formula.class, methodName, params);
 		//Should pass
-		resultBool = (boolean) testMe.invoke(testPrivate, 'a');
+		resultBool = (boolean) testMe.invoke(tester, 'a');
 		assertTrue(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, 'z');
+		resultBool = (boolean) testMe.invoke(tester, 'z');
 		assertTrue(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, 'A');
+		resultBool = (boolean) testMe.invoke(tester, 'A');
 		assertTrue(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, 'Z');
+		resultBool = (boolean) testMe.invoke(tester, 'Z');
 		assertTrue(resultBool);
 		//Should fail
-		resultBool = (boolean) testMe.invoke(testPrivate, '3');
+		resultBool = (boolean) testMe.invoke(tester, '3');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '?');
+		resultBool = (boolean) testMe.invoke(tester, '?');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '*');
+		resultBool = (boolean) testMe.invoke(tester, '*');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '/');
+		resultBool = (boolean) testMe.invoke(tester, '/');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '$');
+		resultBool = (boolean) testMe.invoke(tester, '$');
 		assertFalse(resultBool);
 		
 		//Test isNumBound
 		methodName = "isNumBound";
 		testMe = getMethodOfClass(structure.Formula.class, methodName, params);		
 		//Should pass
-		resultBool = (boolean) testMe.invoke(testPrivate, '+');
+		resultBool = (boolean) testMe.invoke(tester, '+');
 		assertTrue(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '-');
+		resultBool = (boolean) testMe.invoke(tester, '-');
 		assertTrue(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, ' ');
+		resultBool = (boolean) testMe.invoke(tester, ' ');
 		assertTrue(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '(');
+		resultBool = (boolean) testMe.invoke(tester, '(');
 		assertTrue(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, ')');
+		resultBool = (boolean) testMe.invoke(tester, ')');
 		//Should fail
-		resultBool = (boolean) testMe.invoke(testPrivate, '?');
+		resultBool = (boolean) testMe.invoke(tester, '?');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '%');
+		resultBool = (boolean) testMe.invoke(tester, '%');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '$');
+		resultBool = (boolean) testMe.invoke(tester, '$');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, 'j');
+		resultBool = (boolean) testMe.invoke(tester, 'j');
 		assertFalse(resultBool);
 		
 		//Test isNumBound
 		methodName = "isOp";
 		testMe = getMethodOfClass(structure.Formula.class, methodName, params);		
 		//Should pass
-		resultBool = (boolean) testMe.invoke(testPrivate, '+');
+		resultBool = (boolean) testMe.invoke(tester, '+');
 		assertTrue(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '-');
+		resultBool = (boolean) testMe.invoke(tester, '-');
 		assertTrue(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '*');
+		resultBool = (boolean) testMe.invoke(tester, '*');
 		assertTrue(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '/');
+		resultBool = (boolean) testMe.invoke(tester, '/');
 		assertTrue(resultBool);
 		//Should fail
-		resultBool = (boolean) testMe.invoke(testPrivate, '3');
+		resultBool = (boolean) testMe.invoke(tester, '3');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '%');
+		resultBool = (boolean) testMe.invoke(tester, '%');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, '@');
+		resultBool = (boolean) testMe.invoke(tester, '@');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, 'j');
+		resultBool = (boolean) testMe.invoke(tester, 'j');
 		assertFalse(resultBool);
-		resultBool = (boolean) testMe.invoke(testPrivate, 'Z');
+		resultBool = (boolean) testMe.invoke(tester, 'Z');
 		assertFalse(resultBool);
 		
 		//Test calc
@@ -128,29 +147,29 @@ public class UnitTestFormula {
 		params2[2] = double.class;
 		testMe = getMethodOfClass(structure.Formula.class, methodName, params2);
 		//Should pass
-		resultDouble = (double) testMe.invoke(testPrivate, 3.0, '+', 4.0);
+		resultDouble = (double) testMe.invoke(tester, 3.0, '+', 4.0);
 		assertEquals(7.0,resultDouble,0);
-		resultDouble = (double) testMe.invoke(testPrivate, 8.0, '-', 4.0);
+		resultDouble = (double) testMe.invoke(tester, 8.0, '-', 4.0);
 		assertEquals(4.0,resultDouble,0);
-		resultDouble = (double) testMe.invoke(testPrivate, 3.0, '*', 4.0);
+		resultDouble = (double) testMe.invoke(tester, 3.0, '*', 4.0);
 		assertEquals(12.0,resultDouble,0);
-		resultDouble = (double) testMe.invoke(testPrivate, 3.0, '/', 4.0);
+		resultDouble = (double) testMe.invoke(tester, 3.0, '/', 4.0);
 		assertEquals(0.75,resultDouble,0);	
-		resultDouble = (double) testMe.invoke(testPrivate, -1.0, '+', 4.0);
+		resultDouble = (double) testMe.invoke(tester, -1.0, '+', 4.0);
 		assertEquals(3.0,resultDouble,0);
-		resultDouble = (double) testMe.invoke(testPrivate, -7, '-', 4.0);
+		resultDouble = (double) testMe.invoke(tester, -7, '-', 4.0);
 		assertEquals(-11.0,resultDouble,0);
-		resultDouble = (double) testMe.invoke(testPrivate, -3.0, '*', 4.0);
+		resultDouble = (double) testMe.invoke(tester, -3.0, '*', 4.0);
 		assertEquals(-12.0,resultDouble,0);
-		resultDouble = (double) testMe.invoke(testPrivate, -3.0, '/', 4.0);
+		resultDouble = (double) testMe.invoke(tester, -3.0, '/', 4.0);
 		assertEquals(-0.75,resultDouble,0);
-		resultDouble = (double) testMe.invoke(testPrivate, -1.0, '+', -4.0);
+		resultDouble = (double) testMe.invoke(tester, -1.0, '+', -4.0);
 		assertEquals(-5.0,resultDouble,0);
-		resultDouble = (double) testMe.invoke(testPrivate, -7, '-', -4.0);
+		resultDouble = (double) testMe.invoke(tester, -7, '-', -4.0);
 		assertEquals(-3.0,resultDouble,0);
-		resultDouble = (double) testMe.invoke(testPrivate, -3.0, '*', -4.0);
+		resultDouble = (double) testMe.invoke(tester, -3.0, '*', -4.0);
 		assertEquals(12.0,resultDouble,0);
-		resultDouble = (double) testMe.invoke(testPrivate, -3.0, '/', -4.0);
+		resultDouble = (double) testMe.invoke(tester, -3.0, '/', -4.0);
 		assertEquals(0.75,resultDouble,0);	
 		
 		/*//Test toString
@@ -162,6 +181,7 @@ public class UnitTestFormula {
 		
 	}
 	/**
+	 * Helper to test private methods via reflection
 	 * 
 	 * @param testClass The class to be tested
 	 * @param methodName The method to be found
@@ -169,13 +189,20 @@ public class UnitTestFormula {
 	 * @throws NoSuchMethodException Method was not found
 	 * @throws SecurityException
 	 */
-	private Method getMethodOfClass(Class testClass, String methodName, 
-			Class partypes[]) 
-			throws NoSuchMethodException, SecurityException{
+	private Method getMethodOfClass(Class testClass, String methodName, Class partypes[]) 
+	throws NoSuchMethodException, SecurityException{
 		Method method = testClass.getDeclaredMethod(methodName, partypes);
 		method.setAccessible(true);
 		return method;
 	}	
+	
+	/**
+	 * Test exceptions
+	 */
+	@Test
+	public void testExceptions() {
+		
+	}
 	
 	
 	/**
@@ -183,8 +210,6 @@ public class UnitTestFormula {
 	 */
 	@Test
 	public void testCellReferences() throws Exception {
-		table = new Table(5,5);
-		tester = new Formula(null, table);
 		Cell selected;
 		
 		// Select a cell (and check it)
@@ -219,9 +244,7 @@ public class UnitTestFormula {
 	 * Testing multiplication, addition, subtraction, division
 	 */
 	//@Test
-	public void testBasicOperations() throws Exception {
-		tester = new Formula();
-		
+	public void testBasicOperations() throws Exception {		
 		assertEquals(29.0,tester.evaluate("12+17"),0);
 		assertEquals(24.0,tester.evaluate("3*8"),0);
 		assertEquals(14.0,tester.evaluate("17-3"),0);
@@ -232,9 +255,7 @@ public class UnitTestFormula {
 	 * Testing negative numbers
 	 */
 	//@Test
-	public void testNegativeNumbers() throws Exception {
-		tester = new Formula();
-		
+	public void testNegativeNumbers() throws Exception {		
 		assertEquals(-6.0,tester.evaluate("-2*3"),0);
 		assertEquals(-1.0,tester.evaluate("-4/4"),0);
 		assertEquals(-11.0,tester.evaluate("-8-3"),0);
@@ -245,9 +266,7 @@ public class UnitTestFormula {
 	 * Testing negative numbers combined with additional operations
 	 */
 	//@Test
-	public void testNegativeNumbersPlus() throws Exception {
-		tester = new Formula();
-		
+	public void testNegativeNumbersPlus() throws Exception {		
 		assertEquals(6.0,tester.evaluate("-2*-3"),0);
 		assertEquals(1.0,tester.evaluate("-4/-4"),0);
 		assertEquals(-5.0,tester.evaluate("-8--3"),0);
@@ -258,9 +277,7 @@ public class UnitTestFormula {
 	 * Test polynomial operation which have multiple operations included
 	 */
 	//@Test
-	public void testPolynomialOperations() throws Exception {
-		tester = new Formula();
-		
+	public void testPolynomialOperations() throws Exception {		
 		assertEquals(22.0,tester.evaluate("(-2+5)*4+10"),0);
 		assertEquals(32.0,tester.evaluate("2+5*4+10"),0);
 		assertEquals(21.0,tester.evaluate("2+5+4+10"),0);
@@ -271,7 +288,6 @@ public class UnitTestFormula {
 		assertEquals(5.0,tester.evaluate("(5+3*5)/4"),0);
 		assertEquals(20.0,tester.evaluate("((5+3)*(2*5))/4"),0);
 		assertEquals(2.5,tester.evaluate("(100/2/10)/2"),0);
-
 	}
 	
 	/**
@@ -279,8 +295,6 @@ public class UnitTestFormula {
 	 */
 	//@Test
 	public void testMiscOperations() throws Exception {
-		tester = new Formula();
-		
 		assertEquals(17.0,tester.evaluate("8 + 9"),0);
 		assertEquals(2.5,tester.evaluate("(2.5)"),0);
 	}
