8f4b373d723cfa034225ab98689554912b4f0719
Sahil Chaudhary | hybris <sahil.chaudhary@hybris.com>
2014-01-27 00:36:55 -0500

    Added more unit tests for console class

--- test/ui/UnitTestConsole.java
+++ test/ui/UnitTestConsole.java
@@ -4,9 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-
 import org.junit.Test;
-
 import structure.Cell;
 import structure.Table;
 
@@ -21,6 +19,46 @@ public class UnitTestConsole {
 	    expandMethod.invoke(console);
 		assertEquals(10, console.getTable().getLength());
 	}
+	
+	@Test
+	public void testSelect() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		console = new Console();
+		String address = "A1";
+	    Method selectMethod = getMethodOfClass(Console.class, "select");
+	    selectMethod.invoke(console, address);
+	}
+	
+	@Test
+	public void testWelcome() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		console = new Console();
+	    Method welcomeMethod = getMethodOfClass(Console.class, "welcome");
+	    welcomeMethod.invoke(console);
+	}
+	
+	@Test
+	public void testHelp() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		console = new Console();
+	    Method helpMethod = getMethodOfClass(Console.class, "help");
+	    helpMethod.invoke(console);
+	}
+	
+	@Test
+	public void testOpen() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		console = new Console();
+	    Method openMethod = getMethodOfClass(Console.class, "open");
+	    openMethod.invoke(console);
+	    console.getTable().displayTable();
+	}
+	
+	@Test
+	public void testSave() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		console = new Console();
+	    Method saveMethod = getMethodOfClass(Console.class, "save");
+	    saveMethod.invoke(console);
+	    console.getTable().displayTable();
+	}
+
+	
 
 	private Method getMethodOfClass(Class argClass, String argMethodName) {
 	    Method[] methods = argClass.getDeclaredMethods();
