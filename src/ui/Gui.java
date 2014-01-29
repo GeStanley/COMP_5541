1ef7ee793aa085fe90966bce153ae6498711cb69
Mike Burkat <mike.burkat@gmail.com>
2014-01-29 18:17:02 -0500

    commented gui

--- src/ui/Gui.java
+++ src/ui/Gui.java
@@ -8,58 +8,74 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
-public class Gui extends JFrame implements PropertyChangeListener{
-	
+
+/**
+ * This class handles the creation of the gui for the calcul-o-matic program
+ * 
+ * @author Mike
+ */
+public class Gui extends JFrame implements PropertyChangeListener {
+
 	private InputLineComponent inputLine;
 	private JTable spreadsheet;
+
 	
-	public Gui(){
+	/**
+	 * This sets up the Gui aspect by taking the input line and the spreadsheet
+	 * and displaying them.
+	 * 
+	 */
+	public Gui() {
 		// Frame setup
 		super("Calcul-O-Matic");
 		setSize(500, 700);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
-		
+
 		// Grid setup
 		inputLine = new InputLineComponent(null);
 		spreadsheet = new SpreadSheet();
 		JScrollPane scrollPane = new JScrollPane(spreadsheet);
 		spreadsheet.setFillsViewportHeight(true);
-		
+
 		inputLine.addPropertyChangeListener(this);
 		spreadsheet.addPropertyChangeListener(this);
-		
+
 		add(inputLine, BorderLayout.NORTH);
 		add(scrollPane, BorderLayout.CENTER);
-		
-		
-	}
-	
-	public static void main(String args[]){
-		JFrame frame = new Gui();
-		frame.setVisible(true);
+
 	}
 
-	
-	
+	/**
+	 * This handeles the coordination between the input line and the spreadsheet
+	 * 
+	 * An "input" change event comes from the input line, whereas a "select"
+	 * change comes from a change in the selected cell.
+	 */
 	@Override
 	public void propertyChange(PropertyChangeEvent e) {
-		
-		
-		if (e.getPropertyName().equals("input")){
+
+		if (e.getPropertyName().equals("input")) {
 			System.out.println("input");
-			
-			
-			
-		} else if (e.getPropertyName().equals("select")){
+
+		} else if (e.getPropertyName().equals("select")) {
 			System.out.println("select");
 			String s = (String) e.getNewValue();
-			
+
 			inputLine.setText(s);
 		}
-		
-		
+
+	}
+
+	/**
+	 * Initializes the frame, and displays it.
+	 * 
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		JFrame frame = new Gui();
+		frame.setVisible(true);
 	}
 
 }
