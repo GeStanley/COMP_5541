4e62a7a67bd250713779cfe3b5727d2e77ac260d
mikeburkat <mike.burkat@gmail.com>
2014-02-17 17:45:51 -0500

    Added icons that will be used for the buttons

---
+++ src/ui/ButtonComponent.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class ButtonComponent extends JPanel {
+
+	public ButtonComponent() {
+		
+		JButton save = new JButton();
+		
+		
+		
+	}
+	
+}
